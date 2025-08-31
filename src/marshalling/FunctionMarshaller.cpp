527388dd252718918a804d1caded6e15b5e50b8f
Michael DeCicco <mdecicco8@gmail.com>
2025-08-31 03:47:34 -0400

    fix(marshaller): fixed error when passing null for function callbacks to C++

--- src/marshalling/FunctionMarshaller.cpp
+++ src/marshalling/FunctionMarshaller.cpp
@@ -24,13 +24,20 @@ namespace tspp {
         v8::Local<v8::Context> context = callCtx.getContext();
 
         const bind::type_meta& meta = m_dataType->getInfo();
+        u8* data                    = callCtx.alloc(m_dataType);
+
+        if (value->IsNull()) {
+            *((void**)data) = nullptr;
+            return data;
+        }
 
         if (!value->IsFunction()) {
             isolate->ThrowException(
                 v8::Exception::TypeError(v8::String::NewFromUtf8(isolate, "Value is not a function").ToLocalChecked())
             );
 
-            return nullptr;
+            *((void**)data) = nullptr;
+            return data;
         }
 
         void* fptr = Callback::Create(isolate, (bind::FunctionType*)m_dataType, value.As<v8::Function>());
@@ -40,11 +47,11 @@ namespace tspp {
                 v8::Exception::Error(v8::String::NewFromUtf8(isolate, "Failed to create closure").ToLocalChecked())
             );
 
-            return nullptr;
+            *((void**)data) = nullptr;
+            return data;
         }
 
         callCtx.addCallback(fptr);
-        u8* data        = callCtx.alloc(m_dataType);
         *((void**)data) = fptr;
 
         return data;
